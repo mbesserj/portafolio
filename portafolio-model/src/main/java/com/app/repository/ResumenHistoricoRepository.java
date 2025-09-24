91895615378dad9804f982ce0dd4fb2af11290e4
mbesserj <64606876+mbesserj@users.noreply.github.com>
2025-09-24 02:35:10 -0300

    Delete portafolio-model/src/main/java/com/app/repository directory

--- portafolio-model/src/main/java/com/app/repository/ResumenHistoricoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.repository;
-
-import com.app.dto.ResumenHistoricoDto;
-import java.util.List;
-
-public interface ResumenHistoricoRepository {
-    List<ResumenHistoricoDto> obtenerResumenHistorico(Long empresaId, Long custodioId, String cuenta);
-}
--- portafolio-model/src/main/java/com/app/repository/TransaccionRepository.java
+++
@@ -1,7 +0,0 @@
-
-package com.app.repository;
-
-public interface TransaccionRepository {
-    
-    int resetCosteoFlagsByGrupo(Long empresaId, String cuenta, Long custodioId, Long instrumentoId);
-}
